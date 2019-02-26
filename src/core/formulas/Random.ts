60ee70f53c8e3310ee9741e0c51e07f1cbbcd90f
nibeshnodesense <nibesh@nodesense.ai>
2019-02-26 21:28:42 +0530

    min max fixed

--- src/core/formulas/Random.ts
+++ src/core/formulas/Random.ts
@@ -25,10 +25,13 @@ export class Random extends Formula {
       }
 
     run = () => {
-        console.log("Random simulator running");
-        // check min, max range
+        // console.log("Random simulator running");
+        // // check min, max range
         const dataValue = this.device.getDataValue(this.variable.name);
-        dataValue.value = Math.ceil(Math.random() * 100);
+        let value= Math.ceil(Math.random() * this.variable.simulation.max)
+        if(value>=this.variable.simulation.min&&value<=this.variable.simulation.max){
+        dataValue.value = value;
         console.log("Value for " + this.variable.name, " is ", dataValue.value);
+        }
     }
 }
